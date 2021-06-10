d876a36cc62472267656a73494fa28f3815b0c25
Jessica Leite <jessica.mleite@gmail.com>
2021-06-09 22:33:22 -0300

    Pessoa e PessoaRepository

---
+++ MediatorTreinaWeb/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediatorTreinaWeb.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> Get(int id);
+
+        Task Add(T item);
+
+        Task Edit(T item);
+
+        Task Delete(int id);
+    }
+}
