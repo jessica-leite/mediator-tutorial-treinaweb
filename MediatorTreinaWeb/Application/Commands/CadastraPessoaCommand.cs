c6c38c5931c32b906f3382c8625b34b76d781a0b
Jessica Leite <jessica.mleite@gmail.com>
2021-06-26 16:32:01 -0300

    Commands para cadastrar, alterar e exlcuir pessoa

---
+++ MediatorTreinaWeb/Application/Commands/CadastraPessoaCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MediatorTreinaWeb.Application.Commands
+{
+    public class CadastraPessoaCommand : IRequest<string>
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+    }
+}
---
+++ MediatorTreinaWeb/Application/Commands/ExcluiPessoaCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace MediatorTreinaWeb.Application.Commands
+{
+    public class ExcluiPessoaCommand : IRequest<string>
+    {
+        public int Id { get; set; }
+    }
+}
